5d03d83931b508417f78e4a1b7d644b485d821c9
Tugsan84 <nttorunoglu1984@hotmail.com>
2025-10-18 23:25:20 +0300

    Add project files.

---
+++ CalculatorAppToLearnCICDPipeline/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CalculatorAppToLearnCICDPipeline
+{
+    public class Program
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(5, 7));
+        }
+    }
+}
---
+++ CalculatorAppToLearnCICDPipeline/Tests/Test1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CalculatorAppToLearnCICDPipeline;
+using NUnit.Framework;
+
+namespace CalculatorAppToLearnCICDPipeline.Tests
+{
+    public class Test1
+    {
+        [Fact]
+        public void Add_ShouldReturnSumOfTwoNumbers()
+        {
+            // Arrange
+            int x = 3;
+            int y = 2;
+
+            // Act
+            int result = Program.Add(x, y);
+
+            // Assert
+            Assert.Equals(5, result);
+        }
+    }
+}
